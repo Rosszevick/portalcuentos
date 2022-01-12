163d99c25ac33c861471c0f26633bedc2f9d8a88
Rocio Olszevicki <86808467+Rosszevick@users.noreply.github.com>
2022-01-12 19:40:52 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//const user = prompt("Bienvenido a Portal-Cuentos! Ingresa tu nombre de usuario");
+//const pass = prompt("Ingresa tu contraseña");
+
+
+
+//if (user == "Tobias" && pass == "Tobias22" ||
+    //"Tammara" && pass == "Tammy33"||
+    //"Tarico" && pass == "Tabaco5s" ){
+    //alert("Bienvenido " + user+ "Ya puedes entrar!");
+    //}
+    
